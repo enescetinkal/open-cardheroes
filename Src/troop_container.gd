9e4b087c1513b10fd1b612a914329411d289f68a
enescetinkal <151547869+enescetinkal@users.noreply.github.com>
2024-12-11 22:26:18 +0300

    Make a container for troops
    i still haven't make troop appearing work.

--- Src/troop_container.gd
+++ Src/troop_container.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
-var Attack: int = 2
-var Health: int = 2
+@onready var troop: Node2D = $Troop
+
+var troopPlayed = Globals.troopPlayed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if troopPlayed == true: 
+		show()
